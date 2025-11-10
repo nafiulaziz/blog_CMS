1c1487803ec49f886f1e29622c293c555191eee9
Nafiul Aziz <nafiulaz.na@gmail.com>
2025-11-10 12:54:28 +0600

    add a new comment

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ include 'includes/header.php';
     <div class="text-center mb-12">
         <h1 class="text-4xl font-bold text-gray-900 mb-4">Welcome to Blog</h1>
         <p class="text-xl text-gray-600">Discover amazing stories and insights</p>
+        <p class="text-xl text-gray-600">Discover amazing stories and insights</p>
     </div>
 
     <?php if (empty($posts)): ?>
